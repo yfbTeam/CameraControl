9e87cf56c2749dc7e464b69251255bb272c0ecf4
liuzeaa <1098367891@qq.com>
2017-12-06 16:24:06 +0800

    1

---
+++ CameraControl/js/Handel_Js.js
@@ -0,0 +1,35 @@
+﻿var pageUrl = "Handle/CameraControlHandle.ashx";
+
+/*Ajax调用方法*/
+/*data是需要传的json数据 fn匿名函数fn1匿名函数*/
+function getAjax_far_near(data, fn, fn1) {
+   
+    $.ajax({
+        url: pageUrl,
+        type: "post",
+        data:data,
+        async: false,       
+        dataType: "json",        
+        success: fn,
+        error: fn1
+    });
+}
+
+
+
+function getAjax(codetype,datalength, fn, fn1) {
+    var data = {
+        func: 'SendCodeToCamera',
+        codetype: codetype,
+        datalength: datalength,
+    }
+    $.ajax({
+        url: pageUrl,
+        type: "post",
+        data: data,
+        async: false,
+        dataType: "json",
+        success: fn,
+        error: fn1
+    });
+}
